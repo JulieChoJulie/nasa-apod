1eee4bbce6f5da1878106763eb4e08ecddebb354
JulieChoJulie <h32cho@gmail.com>
2019-03-11 15:42:43 -0400

    template.scss fixed for viewer template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import Template from './components/Viewer/Template';
 import SpaceNavigator from './components/SpaceNavigator/SpaceNavigator';
+import Viewer from './components/Viewer/Viewer';
 
 class App extends Component {
     render() {
         return (
             <div>
-              <Template spaceNavigator={<SpaceNavigator />}/>
+              <Template
+                  spaceNavigator={<SpaceNavigator />}
+                  viewer={(
+                      <Viewer
+                          url="https://apod.nasa.gov/apod/image/1712/GeminidsYinHao1024.jpg"
+                          mediaType="image"/>
+                          )}
+              />
             </div>
         );
     }
